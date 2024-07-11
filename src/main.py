1bd331e1753969fd02334a15f26ea5abfabe60bf
Aamir2020 <aamir.zek@gmail.com>
2024-07-10 20:48:30 -0600

    Refactored orbital motion into a class

--- src/main.py
+++ src/main.py
@@ -8,47 +8,69 @@ import numpy as np
 
 
 class celestial_object():
-    # Gravitational Constant
-    Gravitational_Constant = 1
-    tick_rate = 0.01
 
-    def __init__(self, name, coordinate, initial_velocity, mass_of_object):
+    def __init__(self, name, coordinate, velocity, mass):
         self.coordinate = np.array(coordinate, dtype=np.float64)
-        self.initial_velocity = np.array(initial_velocity, dtype=np.float64)
-        self.mass_of_object = float(mass_of_object)
+        self.velocity = np.array(velocity, dtype=np.float64)
+        self.mass = float(mass)
         self.name = name
 
-    def advance_to_next_position(self, list_of_celestial_objects):
-        force = np.array([0, 0], dtype=np.float64)
-        force_angle = 0
-        for body in list_of_celestial_objects:
-            if np.array_equal(self.coordinate, body.coordinate):
-                continue
+    def update_coordinate(self, coordinate):
+        self.coordinate = coordinate
 
-            distance_squared = ((self.coordinate[0] - body.coordinate[0])**2 +
-                                (self.coordinate[1] - body.coordinate[1])**2)
-            if distance_squared == 0:
-                continue
+    def update_velocity(self, velocity):
+        self.velocity = velocity
 
-            magnitude_of_force = self.Gravitational_Constant * \
-                self.mass_of_object*body.mass_of_object / distance_squared
-            force_angle = math.atan2(
-                (body.coordinate[1] - self.coordinate[1]), (body.coordinate[0] - self.coordinate[0]))
-            force[0] += magnitude_of_force*math.cos(force_angle)
-            force[1] += magnitude_of_force*math.sin(force_angle)
 
-        acceleration = force/self.mass_of_object
+class Orbit:
 
-        self.coordinate += self.initial_velocity * \
-            self.tick_rate + 0.5*(acceleration)*self.tick_rate**2
+    # Gravitational Constant
+    Gravitational_Constant = 1
+    tick_rate = 15
 
-        self.initial_velocity += acceleration*self.tick_rate
+    @classmethod
+    def move_celestial_objects(cls, list_of_celestial_objects):
+        list_of_final_velocities = []
+        list_of_final_coordinates = []
 
-        if self.name == "Earth":
-            print(
-                f"Distance={distance_squared:.3}, velocity = {self.initial_velocity}, acceleration={acceleration}, ", end="")
-        if self.name == "Sun":
-            print(f"Angle={force_angle*180/math.pi:.4}")
+        for body_one in list_of_celestial_objects:
+            force = np.array([0, 0], dtype=np.float64)
+            force_angle = 0
+            for body_two in list_of_celestial_objects:
+                if np.array_equal(body_one.coordinate, body_two.coordinate):
+                    continue
+
+                distance_squared = ((body_one.coordinate[0] - body_two.coordinate[0])**2 +
+                                    (body_one.coordinate[1] - body_two.coordinate[1])**2)
+                if distance_squared == 0:
+                    continue
+
+                magnitude_of_force = cls.Gravitational_Constant * \
+                    body_one.mass*body_two.mass / distance_squared
+                force_angle = math.atan2(
+                    (body_two.coordinate[1] - body_one.coordinate[1]), (body_two.coordinate[0] - body_one.coordinate[0]))
+                force[0] += magnitude_of_force*math.cos(force_angle)
+                force[1] += magnitude_of_force*math.sin(force_angle)
+
+            acceleration = force/body_one.mass
+
+            list_of_final_coordinates.append(
+                body_one.coordinate + body_one.velocity*cls.tick_rate + 0.5*(acceleration)*cls.tick_rate**2)
+
+            list_of_final_velocities.append(
+                body_one.velocity + acceleration*cls.tick_rate)
+
+            # if body_one.name == "Earth":
+            #     print(
+            #         f"Distance={distance_squared:.3}, velocity = {self.velocity}, acceleration={acceleration}, ", end="")
+            # if self.name == "Sun":
+            #     print(f"Angle={force_angle*180/math.pi:.4}")
+
+        for index in range(len(list_of_celestial_objects)):
+            list_of_celestial_objects[index].update_velocity(
+                list_of_final_velocities[index])
+            list_of_celestial_objects[index].update_coordinate(
+                list_of_final_coordinates[index])
 
 
 if __name__ == '__main__':
@@ -57,6 +79,6 @@ if __name__ == '__main__':
     Earth = celestial_object("Earth", [100, 0], [0, 1], 1)
 
     while (True):
-        Sun.advance_to_next_position([Earth])
-        Earth.advance_to_next_position([Sun])
-        # time.sleep(0.01)
+        Orbit.move_celestial_objects([Sun, Earth])
+        time.sleep(0.01)
+        print(f"Coordinate = {Earth.coordinate}  Velocity= {Earth.velocity}")
