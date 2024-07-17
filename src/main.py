a33015c8735500037da1568c31180529f1270da8
Aamir2020 <aamir.zek@gmail.com>
2024-07-17 00:09:54 -0600

    Updated Integration method - Updated the leapfrog integration to be in the 'kick-drift-kick' form - Added the outer planets

--- src/main.py
+++ src/main.py
@@ -10,18 +10,26 @@ def update(frame):
     global sun_positions, earth_positions, mars_positions
 
     list_of_final_coordinates = orbit.move_celestial_objects(
-        [Sun, Mercury, Venus, Earth, Mars])
+        [Sun, Mercury, Venus, Earth, Mars, Jupiter, Saturn, Uranus, Neptune])
 
     scat_sun.set_offsets(list_of_final_coordinates[0])
     scat_mercury.set_offsets(list_of_final_coordinates[1])
     scat_venus.set_offsets(list_of_final_coordinates[2])
     scat_earth.set_offsets(list_of_final_coordinates[3])
     scat_mars.set_offsets(list_of_final_coordinates[4])
+    scat_jupiter.set_offsets(list_of_final_coordinates[5])
+    scat_saturn.set_offsets(list_of_final_coordinates[6])
+    scat_uranus.set_offsets(list_of_final_coordinates[7])
+    scat_neptune.set_offsets(list_of_final_coordinates[8])
 
     mercury_positions.append(list_of_final_coordinates[1])
     venus_positions.append(list_of_final_coordinates[2])
     earth_positions.append(list_of_final_coordinates[3])
     mars_positions.append(list_of_final_coordinates[4])
+    jupiter_positions.append(list_of_final_coordinates[5])
+    saturn_positions.append(list_of_final_coordinates[6])
+    uranus_positions.append(list_of_final_coordinates[7])
+    neptune_positions.append(list_of_final_coordinates[8])
 
     mercury_trace.set_data([coordinate[0] for coordinate in mercury_positions],
                            [coordinate[1] for coordinate in mercury_positions])
@@ -31,6 +39,14 @@ def update(frame):
                          [coordinate[1] for coordinate in earth_positions])
     mars_trace.set_data([coordinate[0] for coordinate in mars_positions],
                         [coordinate[1] for coordinate in mars_positions])
+    jupiter_trace.set_data([coordinate[0] for coordinate in jupiter_positions],
+                           [coordinate[1] for coordinate in jupiter_positions])
+    saturn_trace.set_data([coordinate[0] for coordinate in saturn_positions],
+                          [coordinate[1] for coordinate in saturn_positions])
+    uranus_trace.set_data([coordinate[0] for coordinate in uranus_positions],
+                          [coordinate[1] for coordinate in uranus_positions])
+    neptune_trace.set_data([coordinate[0] for coordinate in neptune_positions],
+                           [coordinate[1] for coordinate in neptune_positions])
 
     sun_text.set_position(
         (list_of_final_coordinates[0][0] + text_offset, list_of_final_coordinates[0][1] + text_offset))
@@ -42,8 +58,18 @@ def update(frame):
         (list_of_final_coordinates[3][0] + text_offset, list_of_final_coordinates[3][1] + text_offset))
     mars_text.set_position(
         (list_of_final_coordinates[4][0] + text_offset, list_of_final_coordinates[4][1] + text_offset))
+    jupiter_text.set_position(
+        (list_of_final_coordinates[5][0] + text_offset, list_of_final_coordinates[5][1] + text_offset))
+    saturn_text.set_position(
+        (list_of_final_coordinates[6][0] + text_offset, list_of_final_coordinates[6][1] + text_offset))
+    uranus_text.set_position(
+        (list_of_final_coordinates[7][0] + text_offset, list_of_final_coordinates[7][1] + text_offset))
+    neptune_text.set_position(
+        (list_of_final_coordinates[8][0] + text_offset, list_of_final_coordinates[8][1] + text_offset))
 
-    return scat_sun, scat_mercury, scat_venus, scat_earth, scat_mars, mercury_trace, venus_trace, earth_trace, mars_trace, sun_text, mercury_text, venus_text, earth_text, mars_text
+    return scat_sun, scat_mercury, scat_venus, scat_earth, scat_mars, scat_jupiter, scat_saturn, scat_uranus, scat_neptune, \
+        mercury_trace, venus_trace, earth_trace, mars_trace, jupiter_trace, saturn_trace, uranus_trace, neptune_trace, \
+        sun_text, mercury_text, venus_text, earth_text, mars_text, jupiter_text, saturn_text, uranus_text, neptune_text
 
 
 if __name__ == '__main__':
@@ -70,6 +96,22 @@ if __name__ == '__main__':
         "Mars")
     Mars = celestial_object("Mars", coordinate, vector, mass)
 
+    coordinate, vector, mass = ephemeris_request_handler_impl.send_request(
+        "Jupiter")
+    Jupiter = celestial_object("Jupiter", coordinate, vector, mass)
+
+    coordinate, vector, mass = ephemeris_request_handler_impl.send_request(
+        "Saturn")
+    Saturn = celestial_object("Saturn", coordinate, vector, mass)
+
+    coordinate, vector, mass = ephemeris_request_handler_impl.send_request(
+        "Uranus")
+    Uranus = celestial_object("Uranus", coordinate, vector, mass)
+
+    coordinate, vector, mass = ephemeris_request_handler_impl.send_request(
+        "Neptune")
+    Neptune = celestial_object("Neptune", coordinate, vector, mass)
+
     fig, ax = plt.subplots()
 
     scat_sun = ax.scatter(
@@ -82,6 +124,14 @@ if __name__ == '__main__':
         Earth.coordinate[0], Earth.coordinate[1], c="b", s=20, marker='o')
     scat_mars = ax.scatter(
         Mars.coordinate[0], Mars.coordinate[1], c="b", s=20, marker='o')
+    scat_jupiter = ax.scatter(
+        Jupiter.coordinate[0], Jupiter.coordinate[1], c="b", s=20, marker='o')
+    scat_saturn = ax.scatter(
+        Saturn.coordinate[0], Saturn.coordinate[1], c="b", s=20, marker='o')
+    scat_uranus = ax.scatter(
+        Uranus.coordinate[0], Uranus.coordinate[1], c="b", s=20, marker='o')
+    scat_neptune = ax.scatter(
+        Neptune.coordinate[0], Neptune.coordinate[1], c="b", s=20, marker='o')
 
     sun_text = ax.text(Sun.coordinate[0], Sun.coordinate[1], "Sun")
     mercury_text = ax.text(
@@ -92,19 +142,35 @@ if __name__ == '__main__':
         Earth.coordinate[0] + text_offset, Earth.coordinate[1] + text_offset, "Earth")
     mars_text = ax.text(
         Mars.coordinate[0] + text_offset, Mars.coordinate[1] + text_offset, "Mars")
+    jupiter_text = ax.text(
+        Jupiter.coordinate[0] + text_offset, Jupiter.coordinate[1] + text_offset, "Jupiter")
+    saturn_text = ax.text(
+        Saturn.coordinate[0] + text_offset, Saturn.coordinate[1] + text_offset, "Saturn")
+    uranus_text = ax.text(
+        Uranus.coordinate[0] + text_offset, Uranus.coordinate[1] + text_offset, "Uranus")
+    neptune_text = ax.text(
+        Neptune.coordinate[0] + text_offset, Neptune.coordinate[1] + text_offset, "Neptune")
 
     mercury_trace, = ax.plot([], [], 'k-', lw=1)
     venus_trace, = ax.plot([], [], 'k-', lw=1)
     earth_trace, = ax.plot([], [], 'k-', lw=1)
     mars_trace, = ax.plot([], [], 'k-', lw=1)
+    jupiter_trace, = ax.plot([], [], 'k-', lw=1)
+    saturn_trace, = ax.plot([], [], 'k-', lw=1)
+    uranus_trace, = ax.plot([], [], 'k-', lw=1)
+    neptune_trace, = ax.plot([], [], 'k-', lw=1)
 
     max_positions = 50
     mercury_positions = deque([Mercury.coordinate], maxlen=max_positions)
     venus_positions = deque([Venus.coordinate], maxlen=max_positions)
     earth_positions = deque([Earth.coordinate], maxlen=max_positions)
     mars_positions = deque([Mars.coordinate], maxlen=max_positions)
+    jupiter_positions = deque([Jupiter.coordinate], maxlen=max_positions)
+    saturn_positions = deque([Saturn.coordinate], maxlen=max_positions)
+    uranus_positions = deque([Uranus.coordinate], maxlen=max_positions)
+    neptune_positions = deque([Neptune.coordinate], maxlen=max_positions)
 
-    ax.set(xlim=[-0.25*10**12, 0.25*10**12], ylim=[-0.25*10**12, 0.25*10**12],
+    ax.set(xlim=[-6*10**12, 6*10**12], ylim=[-6*10**12, 6*10**12],
            xlabel='x axis', ylabel='y axis')
     # ax.set_xscale('log')
     # ax.set_yscale('log')
--- src/orbit_builder.py
+++ src/orbit_builder.py
@@ -12,8 +12,11 @@ class orbit:
     def move_celestial_objects(cls, list_of_celestial_objects):
         list_of_final_velocities = []
         list_of_final_coordinates = []
-        list_of_Intermediate_Orbital_State = []
+        list_of_half_step_velocities = []
+        list_of_first_step_orbital_elements = []
+        list_of_second_step_accelerations = []
 
+        # Finds half step velocity and full step orbital elements
         for body_one in list_of_celestial_objects:
             force = np.array([0, 0], dtype=np.float64)
             force_angle = 0
@@ -33,50 +36,56 @@ class orbit:
                 force[0] += magnitude_of_force*math.cos(force_angle)
                 force[1] += magnitude_of_force*math.sin(force_angle)
 
-            acceleration = force/body_one.mass
+            first_step_acceleration = force/body_one.mass
+            first_step_velocity = body_one.coordinate + body_one.velocity * \
+                cls.time_step + 0.5*(first_step_acceleration)*cls.time_step**2
+            first_step_position = body_one.coordinate + body_one.velocity * \
+                cls.time_step + 0.5*(first_step_acceleration)*cls.time_step**2
 
-            intermediate_coordinate = body_one.coordinate + body_one.velocity * \
-                cls.time_step + 0.5*(acceleration)*cls.time_step**2
+            half_step_velocity = body_one.velocity + \
+                first_step_acceleration*cls.time_step/2
 
-            intermediate_velocities = body_one.velocity + acceleration*cls.time_step
+            list_of_half_step_velocities.append(half_step_velocity)
+            list_of_first_step_orbital_elements.append(
+                intermediate_orbital_state(first_step_position, first_step_velocity))
 
-            list_of_Intermediate_Orbital_State.append(
-                intermediate_orbital_state(intermediate_coordinate, body_one.coordinate, intermediate_velocities, body_one.velocity, force, body_one.mass))
-
-        for body_one in list_of_Intermediate_Orbital_State:
+        # Finds acceleration for second step
+        for body_one_index in range(len(list_of_first_step_orbital_elements)):
             force = np.array([0, 0], dtype=np.float64)
             force_angle = 0
-            for body_two in list_of_Intermediate_Orbital_State:
-                if np.array_equal(body_one.intermediate_coordinate, body_two.intermediate_coordinate):
+            for body_two_index in range(len(list_of_first_step_orbital_elements)):
+                if np.array_equal(list_of_first_step_orbital_elements[body_one_index].intermediate_coordinate, list_of_first_step_orbital_elements[body_two_index].intermediate_coordinate):
                     continue
 
-                distance_squared = ((body_one.intermediate_coordinate[0] - body_two.intermediate_coordinate[0])**2 +
-                                    (body_one.intermediate_coordinate[1] - body_two.intermediate_coordinate[1])**2)
+                distance_squared = ((list_of_first_step_orbital_elements[body_one_index].intermediate_coordinate[0] - list_of_first_step_orbital_elements[body_two_index].intermediate_coordinate[0])**2 +
+                                    (list_of_first_step_orbital_elements[body_one_index].intermediate_coordinate[1] - list_of_first_step_orbital_elements[body_two_index].intermediate_coordinate[1])**2)
                 if distance_squared == 0:
                     continue
 
                 magnitude_of_force = cls.Gravitational_Constant * \
-                    body_one.mass*body_two.mass / distance_squared
+                    list_of_celestial_objects[body_one_index].mass * \
+                    list_of_celestial_objects[body_two_index].mass / \
+                    distance_squared
                 force_angle = math.atan2(
-                    (body_two.intermediate_coordinate[1] - body_one.intermediate_coordinate[1]), (body_two.intermediate_coordinate[0] - body_one.intermediate_coordinate[0]))
+                    (list_of_first_step_orbital_elements[body_two_index].intermediate_coordinate[1] - list_of_first_step_orbital_elements[body_one_index].intermediate_coordinate[1]), (list_of_first_step_orbital_elements[body_two_index].intermediate_coordinate[0] - list_of_first_step_orbital_elements[body_one_index].intermediate_coordinate[0]))
                 force[0] += magnitude_of_force*math.cos(force_angle)
                 force[1] += magnitude_of_force*math.sin(force_angle)
 
-            average_force = 0.5*(force + body_one.force)
+            second_step_acceleration = force / \
+                list_of_celestial_objects[body_one_index].mass
+            list_of_second_step_accelerations.append(second_step_acceleration)
 
-            acceleration = average_force/body_one.mass
+        # Combines all the results for implementing 'kick-drift-kick' form
+        for body_one_index in range(len(list_of_celestial_objects)):
 
-            list_of_final_coordinates.append(
-                body_one.initial_coordinate + body_one.initial_velocity*cls.time_step + 0.5*(acceleration)*cls.time_step**2)
+            first_step_position = list_of_celestial_objects[body_one_index].coordinate + \
+                list_of_half_step_velocities[body_one_index]*cls.time_step
+            first_step_velocity = list_of_half_step_velocities[body_one_index] + \
+                list_of_second_step_accelerations[body_one_index] * \
+                cls.time_step/2
 
-            list_of_final_velocities.append(
-                body_one.initial_velocity + acceleration*cls.time_step)
-
-            # if body_one.name == "Earth":
-            #     print(
-            #         f"Distance={distance_squared:.3}, velocity = {self.velocity}, acceleration={acceleration}, ", end="")
-            # if self.name == "Sun":
-            #     print(f"Angle={force_angle*180/math.pi:.4}")
+            list_of_final_coordinates.append(first_step_position)
+            list_of_final_velocities.append(first_step_velocity)
 
         for index in range(len(list_of_celestial_objects)):
             list_of_celestial_objects[index].update_velocity(
@@ -89,15 +98,8 @@ class orbit:
 
 class intermediate_orbital_state:
 
-    def __init__(self, intermediate_coordinate, initial_coordinate, intermediate_velocity, initial_velocity, force, mass):
+    def __init__(self, intermediate_coordinate, intermediate_velocity):
         self.intermediate_coordinate = np.array(
             intermediate_coordinate, dtype=np.float64)
-        self.initial_coordinate = np.array(
-            initial_coordinate, dtype=np.float64)
         self.intermediate_velocity = np.array(
             intermediate_velocity, dtype=np.float64)
-        self.initial_velocity = np.array(
-            initial_velocity, dtype=np.float64)
-        self.force = np.array(
-            force, dtype=np.float64)
-        self.mass = float(mass)
