0c044642ff5757c9f79e9929591bcb2674873fb5
Aamir2020 <aamir.zek@gmail.com>
2024-08-12 04:01:21 -0600

    working model but doesn't scale with zoom

--- src/celestial_objects_plot.py
+++ src/celestial_objects_plot.py
@@ -11,6 +11,7 @@ class celestial_objects_plot:
     text_offset = 10**10
     target = None
     text_rect = None
+    count = 1
 
     @classmethod
     def set_target_body_to_center(cls, celestial_object):
@@ -34,7 +35,6 @@ class celestial_objects_plot:
         self.celestial_object_trace, = ax.plot([], [], 'k-', lw=1)
         self.celestial_object_positions = deque(
             [self.celestial_object.coordinate], maxlen=self.max_positions)
-        self.update_text_boundary()
 
     def set_plot_offsets(self, final_coordinate):
         self.celestial_object_scatter_plot.set_offsets(final_coordinate)
@@ -47,8 +47,15 @@ class celestial_objects_plot:
                                              [coordinate[1] for coordinate in self.celestial_object_positions])
 
     def update_text_position(self, final_coordinate):
+        if self.count > 0:
+            bbox = self.get_text_bbox()
+            self.bbox_template = bounding_box_template(
+                bbox.x0, bbox.x1, bbox.width, bbox.height)
+            self.count -= 1
         self.celestial_object_text.set_position(
             (final_coordinate[0], final_coordinate[1] + self.text_offset))
+        self.bbox_template.update_position(
+            final_coordinate[0], final_coordinate[1] + self.text_offset)
 
     def get_celestial_object(self):
         return self.celestial_object
@@ -69,8 +76,8 @@ class celestial_objects_plot:
         return bboxbase.transformed(self.ax.transData.inverted())
 
     def is_text_overlapping(self, other_celestial_plot):
-        this_bbox = self.get_text_bbox()
-        other_bbox = other_celestial_plot.get_text_bbox()
+        this_bbox = self.bbox_template
+        other_bbox = other_celestial_plot.get_bbox_template()
 
         # Check if the bounding boxes overlap
         overlap = not (this_bbox.x0 > other_bbox.x1 or
@@ -83,7 +90,25 @@ class celestial_objects_plot:
     def update_text_boundary(self):
         if self.text_rect:
             self.text_rect.remove()  # Remove previous rectangle
-        bbox = self.get_text_bbox()
-        self.text_rect = Rectangle((bbox.x0, bbox.y0), bbox.width, bbox.height,
+        self.text_rect = Rectangle((self.bbox_template.x0, self.bbox_template.y0), self.bbox_template.width, self.bbox_template.height,
                                    linewidth=1, edgecolor='r', facecolor='none')
         self.ax.add_patch(self.text_rect)
+
+    def get_bbox_template(self):
+        return self.bbox_template
+
+
+class bounding_box_template:
+    def __init__(self, x, y, width, height):
+        self.x0 = x
+        self.x1 = x + width
+        self.y0 = y
+        self.y1 = y + height
+        self.width = width
+        self.height = height
+
+    def update_position(self, x, y):
+        self.x0 = x
+        self.x1 = x + self.width
+        self.y0 = y
+        self.y1 = y + self.height
